2ecec06b4fb7160a9593775e02760a2b80a6bb96
masudrana417 <masudrana71@gmail.com>
2024-12-31 13:19:23 +0600

    31 Dec 2024

---
+++ vue-project2/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  
+  <NewYear/>
+  <FruitItems/>
+</template>
+
+<script>
+
+import NewYear from './components/NewYear.vue'
+import FruitItems from './components/FruitItems.vue'
+
+
+export default {
+  name: 'App',
+  components: {
+    NewYear, FruitItems
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vue-project2/src/components/NewYear.vue
@@ -0,0 +1,39 @@
+<template>
+    <div>
+        <h1 v-bind:class="ClassName">Happy New Year 2025</h1>
+        <img v-bind:src="url" alt="" width="700" height="450"><hr>
+        <p v-if="ProductsInstock">
+    in stock
+    </p>
+    <p v-else>
+    not in stock
+    </p>
+
+    <div v-show="showDiv">This div tag can be hidden</div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                ClassName: "head",
+                url: "newYear.jpg",   //src path image
+                //url:require('@/assets/newYear.png')  //assets path image
+                ProductsInstock:false,
+                showDiv:true
+            }
+        }
+        
+    }
+</script>
+
+<style>
+.head{
+    color:black;
+    background-color: orange;
+    font-size: 50px;
+    text-align: center;
+    margin-top: 100px;
+}
+</style>
