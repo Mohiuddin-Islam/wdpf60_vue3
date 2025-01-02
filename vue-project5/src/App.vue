49d47970a50b03011e5cd7cc9d5193a8bff76ae5
masudrana417 <masudrana71@gmail.com>
2025-01-02 13:32:02 +0600

    02 Jan 2025

---
+++ vue-project5/src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { RouterLink, RouterView } from "vue-router";
+import HeaderView from "./components/HeaderView.vue";
+
+
+</script>
+
+<template>
+  <HeaderView/>
+  
+    <RouterView />
+
+
+</template>
